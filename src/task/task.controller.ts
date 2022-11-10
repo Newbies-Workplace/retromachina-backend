9053d04575752d57cd71d170e310eae3e057f138
TacticalCrab <pkaleksandrowicz@gmail.com>
2022-11-10 11:05:49 +0100

    WIP

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('tasks')
 export class TaskController {
+  constructor(private taskService: TaskService) {}
+
   @Get()
-  getTasks(@Query('retro_id') retroID: string) {}
+  getTasks(@Query('retro_id') retroID: string) {
+    if (retroID.trim().length === 0)
+      throw new BadRequestException('No query param');
+  }
 }
