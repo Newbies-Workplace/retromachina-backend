990b3dc38f50bb996b6be19bbb328f43dfa2f736
TacticalCrab <pkaleksandrowicz@gmail.com>
2022-11-23 13:39:08 +0100

    fix interfaces

---
+++ src/gateway/interfaces/retroRoom.interface.ts
@@ -0,0 +1,27 @@
+
+export interface Card {
+    id: string;
+    text: string;
+    authorId: string;
+    columnId: string;
+}
+
+export interface User {
+    userId: string;
+    isReady: boolean;
+    isWriting: boolean;
+}
+export interface ScrumMaster {
+    userId: string;
+}
+
+export interface RetroColumn {
+    id: string;
+    color: string;
+    name: string;
+    description: string;
+    cards: Card[];
+    teamCardsAmount: number;
+    usersWriting: number;
+    isWriting: boolean;
+}
