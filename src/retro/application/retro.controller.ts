ffb6bec53ee93e6fa38c78004602784d9c262c8c
JUtupe <jutuper@gmail.com>
2023-12-13 17:41:11 +0100

    feat: team ownership

--- src/retro/application/retro.controller.ts
+++ src/retro/application/retro.controller.ts
@@ -7,59 +7,60 @@ import {
   Post,
   Query,
   UseGuards,
-} from '@nestjs/common';
-import { JwtGuard } from 'src/auth/jwt/jwt.guard';
-import { RetroService } from '../domain/retro.service';
-import { User } from 'src/auth/jwt/jwtuser.decorator';
-import { JWTUser } from 'src/auth/jwt/JWTUser';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { RetroCreateRequest } from './model/retro.request';
-import { AuthAbilityFactory } from '../../auth/auth.ability';
-import { ForbiddenError, subject } from '@casl/ability';
-import { RetroResponse } from './model/retro.response';
-import { toRetroResponse } from './retro.converter';
+} from "@nestjs/common";
+import { JwtGuard } from "src/auth/jwt/jwt.guard";
+import { RetroService } from "../domain/retro.service";
+import { User } from "src/auth/jwt/jwtuser.decorator";
+import { JWTUser } from "src/auth/jwt/JWTUser";
+import { PrismaService } from "src/prisma/prisma.service";
+import { RetroCreateRequest } from "./model/retro.request";
+import { AuthAbilityFactory } from "../../auth/auth.ability";
+import { ForbiddenError, subject } from "@casl/ability";
+import { RetroResponse } from "./model/retro.response";
+import { toRetroResponse } from "./retro.converter";
 
-@Controller('retros')
+@Controller("retros")
 export class RetroController {
   constructor(
     private retroService: RetroService,
     private prismaService: PrismaService,
-    private abilityFactory: AuthAbilityFactory,
+    private abilityFactory: AuthAbilityFactory
   ) {}
 
   @Get()
   @UseGuards(JwtGuard)
   async getTeamRetros(
     @User() user: JWTUser,
-    @Query('team_id') teamId: string,
+    @Query("team_id") teamId: string
   ): Promise<RetroResponse[]> {
-    if (teamId.trim().length === 0) throw new BadRequestException('No `team_id` query param');
+    if (teamId.trim().length === 0)
+      throw new BadRequestException("No `team_id` query param");
     const team = await this.prismaService.team.findUniqueOrThrow({
       where: {
         id: teamId,
-      }
+      },
     });
     const ability = this.abilityFactory.create(user);
 
-    ForbiddenError.from(ability).throwUnlessCan('read', subject('Team', team));
+    ForbiddenError.from(ability).throwUnlessCan("read", subject("Team", team));
 
     const retros = await this.prismaService.retrospective.findMany({
       where: {
         team_id: teamId,
       },
       orderBy: {
-        date: 'desc',
+        date: "desc",
       },
     });
 
     return retros.map(toRetroResponse);
   }
 
-  @Get(':id')
+  @Get(":id")
   @UseGuards(JwtGuard)
   async getRetro(
     @User() user: JWTUser,
-    @Param('id') retroId: string
+    @Param("id") retroId: string
   ): Promise<RetroResponse> {
     const retro = await this.prismaService.retrospective.findUniqueOrThrow({
       where: {
@@ -68,9 +69,11 @@ export class RetroController {
     });
     const ability = this.abilityFactory.create(user);
 
-    ForbiddenError.from(ability).throwUnlessCan('read', subject('Retro', retro));
+    ForbiddenError.from(ability).throwUnlessCan(
+      "read",
+      subject("Retro", retro)
+    );
 
-    // todo add index to retro
     return toRetroResponse(retro);
   }
 
@@ -80,18 +83,21 @@ export class RetroController {
     @User() user: JWTUser,
     @Body() request: RetroCreateRequest
   ): Promise<RetroResponse> {
-    const team =  await this.prismaService.team.findUniqueOrThrow({
+    const team = await this.prismaService.team.findUniqueOrThrow({
       where: {
         id: request.teamId,
       },
     });
     const ability = this.abilityFactory.create(user);
 
-    ForbiddenError.from(ability).throwUnlessCan('startRetro', subject('Team', team));
+    ForbiddenError.from(ability).throwUnlessCan(
+      "startRetro",
+      subject("Team", team)
+    );
 
     await this.assertNotRunningRetro(team.id);
 
-    const retro = await this.retroService.createRetro(user.id, request)
+    const retro = await this.retroService.createRetro(user.id, request);
 
     return toRetroResponse(retro);
   }
@@ -105,7 +111,7 @@ export class RetroController {
     });
     if (runningRetro) {
       throw new BadRequestException(
-        'One retro for this team is already running.',
+        "One retro for this team is already running."
       );
     }
   }
